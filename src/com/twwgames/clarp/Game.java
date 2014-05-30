5ed7767ca1dc28b6d994c0ae78bc6806e3668e03
Brittany West <bkwest@ucsc.edu>
2014-05-30 01:47:21 -0700

    -Implemented a ListView and the ArrayList and ArrayAdapter that will inflate our ListView with stuff (nothings in the arraylist) -Added a Game class -renamed MainActivity to StartActivity -Added a GameActivity, which is blank -Added intents so that, hypothetically, you should be able to navigate from the StartActivity to GameActivity if there's something in the ListView

---
+++ src/com/twwgames/clarp/Game.java
@@ -0,0 +1,41 @@
+package com.twwgames.clarp;
+
+public class Game {
+    private String name;
+    private Integer id;
+    private Integer type; // integer codes: 1 for murder mystery
+    private String description;
+    //private ArrayList users;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/com/twwgames/clarp/StartActivity.java
@@ -0,0 +1,106 @@
+package com.twwgames.clarp;
+
+import java.util.ArrayList;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class StartActivity extends ActionBarActivity {
+
+    private ListView gamesListView;
+    private ArrayList<Game> gamesList;
+    private ArrayAdapter<Game> arrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+            .add(R.id.container, new PlaceholderFragment()).commit();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        gamesListView = (ListView) findViewById(R.id.games_list_view);
+        gamesListView.setEmptyView(findViewById(R.id.empty_games_view));
+
+        gamesListView.setOnItemClickListener(new OnItemClickListener() {
+
+            // user clicks to go to a select screen where they can choose
+            // to view any list
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                    int position, long id) {
+                Intent intent = new Intent(StartActivity.this,
+                        GameActivity.class);
+                Game game = gamesList.get((int) id);
+                intent.putExtra("game_id", game.getId());
+                startActivity(intent);
+
+            }
+        });
+
+        gamesList = new ArrayList<Game>();
+
+        arrayAdapter = new ArrayAdapter<Game>(this,
+                android.R.layout.simple_list_item_1, gamesList);
+        gamesListView.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container,
+                    false);
+            return rootView;
+        }
+    }
+
+}
